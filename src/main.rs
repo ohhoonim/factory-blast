bfaa59669be58109d411b6f64b7171998649b64f
코딩하는오후 <hyounddon.ju@gmail.com>
2026-08-19 09:45:46 +0900

    웹 컴포넌트 적용해보기 Fixes #1

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use factory_blast::App;
+use leptos::prelude::*;
+
+fn main() {
+    // set up logging
+    _ = console_log::init_with_level(log::Level::Debug);
+    console_error_panic_hook::set_once();
+
+    mount_to_body(|| {
+        view! { <App /> }
+    })
+}
