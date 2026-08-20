659a27ca4c9b1d95cbb4d163c18d3ead4e86b46f
코딩하는오후 <hyounddon.ju@gmail.com>
2026-08-20 17:07:20 +0900

    로그인 페이지 만들기 연습 Fixes #3

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ mod pages;
 
 // Top-Level pages
 use crate::pages::home::Home;
+use crate::pages::login::Login;
 
 /// An app router which renders the homepage and handles 404's
 #[component]
@@ -28,6 +29,7 @@ pub fn App() -> impl IntoView {
         <Router>
             <Routes fallback=|| view! { NotFound }>
                 <Route path=path!("/") view=Home />
+                <Route path=path!("/login") view=Login />
             </Routes>
         </Router>
     }
